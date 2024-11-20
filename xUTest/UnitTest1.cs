67433ec866c2bf7a0c5f5ef29867b51a27023a03
LAM QUOC BAO <4701104048@student.hcmue.edu.vn>
2024-11-20 23:04:47 +0700

    m

--- xUTest/UnitTest1.cs
+++ xUTest/UnitTest1.cs
@@ -6,7 +6,7 @@
         [Fact]
         public void Test1()
         {
-            Assert.True(1 + 1 == 3);
+            Assert.True(1 + 1 == 2);
         }
 
         // Test case 2: Kiểm tra một giá trị bằng false
